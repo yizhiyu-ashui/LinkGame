898d10a2dda61a766b62a2be23eeac1426cc66a0
yizhiyu <742990450@qq.com>
2019-02-13 10:40:41 +0800

    创建项目，新建场景
    创建新的工程项目，取名为”LinkGame“，新建三个文件夹：Resources，Scenes，Scripts，Resources里面再建Sounds，UI；再新建三个场景保存在Scenes，分别取名为：StartScene，LevelScene，GameScene，在场景StartScene中新建背景和开始按钮，并新建StartManager.cs脚本，控制场景跳转，最后为开始按钮添加跳转事件。

---
+++ LinkGame/Assets/Scripts/StartManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 场景开始管理类
+/// scy/2019-02-13
+/// </summary>
+public class StartManager : MonoBehaviour {
+
+    public void StartGame(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
